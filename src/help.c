ff5bf95af898bfde935bb01b47a43aaea12a909d
Eric Normandeau <eric.normandeau.qc@gmail.com>
2014-09-24 21:21:16 -0400

    Improved help message

--- src/help.c
+++ src/help.c
@@ -2,9 +2,17 @@
 
 void help(void) {
     puts("\
-    Swoosh, a basic rocket flight simulator\n\
+    Lawn Dart: Model Rocket Flight Simulator\n\
     \n\
     Usage:\n\
-        ./swoosh mass diam cd avgimp totimp wind duration\n");
+        lawn_dart mass diam cd avgimp totimp wind duration\n\
+    \n\
+    mass = Mass of rocket in grams\n\
+    diam = Diameter of body tube in millimeters\n\
+    cd   = Coefficient of penetration (0.6 to 0.9)\n\
+    avgimp (will disapear)\n\
+    totimp (will disapear)\n\
+    wind = Speed of wind in kilometers per hour\n\
+    duration = Maximum duration of simulation\n");
 }
 
