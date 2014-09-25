ba1300f6095adac53679be8d530670c13409e68e
Eric Normandeau <eric.normandeau.qc@gmail.com>
2014-09-24 21:27:49 -0400

    Help formatting

--- src/help.c
+++ src/help.c
@@ -1,18 +1,17 @@
 # include <stdio.h>
 
 void help(void) {
-    puts("\
-    Lawn Dart: Model Rocket Flight Simulator\n\
-    \n\
-    Usage:\n\
-        lawn_dart mass diam cd avgimp totimp wind duration\n\
-    \n\
-    mass = Mass of rocket in grams\n\
-    diam = Diameter of body tube in millimeters\n\
-    cd   = Coefficient of penetration (0.6 to 0.9)\n\
-    avgimp (will disapear)\n\
-    totimp (will disapear)\n\
-    wind = Speed of wind in kilometers per hour\n\
-    duration = Maximum duration of simulation\n");
+    puts("Lawn Dart: Model Rocket Flight Simulator\n\
+\n\
+Usage:\n\
+    lawn_dart mass diam cd avgimp totimp wind duration\n\
+\n\
+mass = Mass of rocket in grams\n\
+diam = Diameter of body tube in millimeters\n\
+cd   = Coefficient of penetration (0.6 to 0.9)\n\
+avgimp (will disapear)\n\
+totimp (will disapear)\n\
+wind = Speed of wind in kilometers per hour\n\
+duration = Maximum duration of simulation\n");
 }
 
