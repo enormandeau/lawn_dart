16566ce9af2c5b3c6746304fb7550be34181a4da
enormandeau <eric.normandeau.qc@gmail.com>
2014-09-29 13:53:44 -0400

    Moved help to messages

---
+++ src/messages.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+void print_help(void) {
+    puts("Lawn Dart: Model Rocket Flight Simulator\n\
+\n\
+Usage:\n\
+    lawn_dart mass diam cd avgimp totimp wind duration\n\
+\n\
+mass = Mass of rocket (g)\n\
+diam = Diameter of body tube (mm)\n\
+cd   = Coefficient of penetration (0.6 to 0.9)\n\
+wind = Speed of wind (km/²)\n");
+}
+
